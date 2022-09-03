8f17c0f7dd3db3f72f634ef91d2850de0714ee1e
muhammed faisan <azeezfaizan@gmail.com>
2022-09-03 13:34:15 +0530

    i

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -15,4 +15,34 @@ export class CalculatorComponent implements OnInit {
   clickme() {
     console.log('it does nothing',this.username);
   }
+
+   dis(val:any) {
+    this.username += val
+}
+
+num:any
+operator:any
+y:any
+x:any
+ oper(op: any) {
+   this.num = this.username 
+    this.operator = op
+    this.username =''
+}
+
+ equalto(op: any) {
+    this.x = this.username 
+    this.num  = this.username 
+    this.operator = op.username 
+    if (this.operator == '+') {
+       this.y = this.num + this.x 
+    } else if (this.operator == '-') {
+      this.y  = this.num - this.x 
+    } else if (this.operator == '*') {
+      this.y  = this.num * this.x 
+    } else if (this.operator == '/') {
+      this.y  = this.num / this.x 
+    }
+    this.username = this.y 
+}
 }
